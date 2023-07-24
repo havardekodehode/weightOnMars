b95aa87000eda55b58f81b494b7c74b8c19610f3
havardekodehode <havarde.kodehode@gmail.com>
2023-07-24 08:41:20 +0200

    Functional

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function language(){
     
 }
 
-const solarSystem = [{planetName:"Mercury", relativeGravity:0.38}, 
+let solarSystem = document.getElementById('noHeroText').hidden = true ?
+                    [{planetName:"Mercury", relativeGravity:0.38}, 
                      {planetName:"Venus", relativeGravity:0.91}, 
                      {planetName:"Earth", relativeGravity:1}, 
                      {planetName:"Mars", relativeGravity:0.38}, 
@@ -15,7 +16,16 @@ const solarSystem = [{planetName:"Mercury", relativeGravity:0.38},
                      {planetName:"Saturn", relativeGravity:1.06}, 
                      {planetName:"Uranus", relativeGravity:1.19},
                      {planetName:"Neptune", relativeGravity:0.38}
-                    ];
+                    ]:
+                    [{planetName:"Merkur", relativeGravity:0.38}, 
+                    {planetName:"Venus", relativeGravity:0.91}, 
+                    {planetName:"Jorden", relativeGravity:1}, 
+                    {planetName:"Mars", relativeGravity:0.38}, 
+                    {planetName:"Jupiter", relativeGravity:2.34}, 
+                    {planetName:"Saturn", relativeGravity:1.06}, 
+                    {planetName:"Uranus", relativeGravity:1.19},
+                    {planetName:"Neptun", relativeGravity:0.38}
+                   ];
 
 inputEl.onchange = (event) => {
     planetsEl.innerHTML = ""
@@ -30,6 +40,20 @@ inputEl.onchange = (event) => {
     });
 }
 
+enButton.addEventListener("click", function(){
+    if(document.getElementById('enHeroText').hidden = true){
+        document.getElementById('noHeroText').hidden = true
+        document.getElementById('enHeroText').hidden = false
+        document.getElementById('noMainText').hidden = true
+        document.getElementById('enMainText').hidden = false
+    }
+    
+})
 noButton.addEventListener("click", function(){
-
+    if(document.getElementById('noHeroText').hidden = true){
+        document.getElementById('enHeroText').hidden = true
+        document.getElementById('noHeroText').hidden = false
+        document.getElementById('enMainText').hidden = true
+        document.getElementById('noMainText').hidden = false
+    }
 })
