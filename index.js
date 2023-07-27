7c94d22848a79aee1e0b0c3e241236cd280c0af4
havardekodehode <havarde.kodehode@gmail.com>
2023-07-27 08:51:18 +0200

    some changes

--- index.js
+++ index.js
@@ -1,81 +1,118 @@
 const inputEl = document.getElementById("input")
 const planetsEl = document.getElementById("planets")
-const noButton = document.getElementById("no");
-const enButton = document.getElementById("en");
+const noRadio = document.getElementById("no");
+const enRadio = document.getElementById("en");
+const radios = document.querySelectorAll('input[name="language"]')
+let textEng = document.querySelectorAll(".eng")
+let textNor = document.querySelectorAll(".nor")
+
 
 function language(){
     
 }
 
-let solarSystem = document.getElementById('noHeroText').hidden === true ?
-                    [{planetName:"Mercury", relativeGravity:0.38}, 
-                     {planetName:"Venus", relativeGravity:0.91}, 
-                     {planetName:"Earth", relativeGravity:1}, 
-                     {planetName:"Mars", relativeGravity:0.38}, 
-                     {planetName:"Jupiter", relativeGravity:2.34}, 
-                     {planetName:"Saturn", relativeGravity:1.06}, 
-                     {planetName:"Uranus", relativeGravity:1.19},
-                     {planetName:"Neptune", relativeGravity:0.38}
-                    ]:
-                    [{planetName:"Merkur", relativeGravity:0.38}, 
-                    {planetName:"Venus", relativeGravity:0.91}, 
-                    {planetName:"Jorden", relativeGravity:1}, 
-                    {planetName:"Mars", relativeGravity:0.38}, 
-                    {planetName:"Jupiter", relativeGravity:2.34}, 
-                    {planetName:"Saturn", relativeGravity:1.06}, 
-                    {planetName:"Uranus", relativeGravity:1.19},
-                    {planetName:"Neptun", relativeGravity:0.38}
-                   ];
+let solarSystem = 
+                    [{planetName:"Mercury", planetNameNor:"Merkur", relativeGravity:0.38}, 
+                     {planetName:"Venus", planetNameNor:"Venus", relativeGravity:0.91}, 
+                     {planetName:"Earth", planetNameNor:"Jorden", relativeGravity:1}, 
+                     {planetName:"Mars", planetNameNor:"Mars", relativeGravity:0.38}, 
+                     {planetName:"Jupiter", planetNameNor:"Jupiter", relativeGravity:2.34}, 
+                     {planetName:"Saturn", planetNameNor:"Saturn", relativeGravity:1.06}, 
+                     {planetName:"Uranus", planetNameNor:"Uranus", relativeGravity:1.19},
+                     {planetName:"Neptune", planetNameNor:"Neptun", relativeGravity:0.38}
+                    ]
 
 inputEl.oninput = (event) => {
     planetsEl.innerHTML = ""
     solarSystem.forEach(planet => {
-        // createElemet
-        planetsEl.innerHTML+= ` 
-        <div class="planet">
-            <img id="planetImage" src="images/${planet.planetName}.webp" alt="Image of ${planet.planetName}">
-            <h3>${planet.planetName}: ${(Number(inputEl.value) * planet.relativeGravity).toFixed(2)}Kg </h3>
-        </div>
-        `
+        const div = document.createElement("div")
+
+        const img = document.createElement("img")
+        img.id="planetImage"
+        img.src=`images/${planet.planetName}.webp`
+        
+
+        const h3 = document.createElement("h3")
+        const h3Nor = document.createElement("h3")
+        h3.append(`${planet.planetName}: ${(Number(inputEl.value) * planet.relativeGravity).toFixed(2)}Kg`)
+        h3.classList.add("eng")
+        h3.style = "margin-top: 0px"
+        // h3Nor.style = "margin-bottom: 4px"
+
+        h3Nor.append(`${planet.planetNameNor}: ${(Number(inputEl.value) * planet.relativeGravity).toFixed(2)}Kg`)
+        h3Nor.classList.add("nor")
+        h3Nor.style = "margin-top: 0px"
+        // h3Nor.style = "margin-bottom: 4px"
+
+        div.append(img, h3, h3Nor)
+
+        planetsEl.append(div)
+        textEng = document.querySelectorAll(".eng")
+        textNor = document.querySelectorAll(".nor")
+        // planetsEl.innerHTML+= ` 
+        // <div class="planet">
+        //     <img id="planetImage" src="images/${planet.planetName}.webp" alt="Image of ${planet.planetName}" style="animation:fadeIn 5s, spin 30s cubic-bezier(0, 0, 1, 1) infinite ">
+        //     <h3>${enRadio.checked ? planet.planetName:planet.planetNameNor}: ${(Number(inputEl.value) * planet.relativeGravity).toFixed(2)}Kg </h3>
+        // </div>
+        // `
+
+
     });
 }
 
-enButton.addEventListener("click", function(){
-    if(document.getElementById('enHeroText').hidden = true){
-        document.getElementById('noHeroText').hidden = true
-        document.getElementById('enHeroText').hidden = false
-        document.getElementById('noMainText').hidden = true
-        document.getElementById('enMainText').hidden = false
-    }
+// const css = document.styleSheets[0];
+
+
+
+for(let i = 0; i < radios.length; i++){
+    radios[i].addEventListener("change", function(){
+        let val = this.value
+        console.log(val);
+        if(val === "en"){
+            for(let item of textNor){
+                item.style = "visibility: hidden"
+            } 
+            for(let item of textEng){
+                item.style = "visibility: visible"
+            }
+        }else if(val === "no"){
+            for(let item of textEng){
+                item.style = "visibility: hidden"
+            }
+            for(let item of textNor){
+                item.style = "visibility: visible"
+            } 
+        }
+    })
+}
+
+// if(enRadio.checked){
+//     for(let item of textEng){
+//         console.log(item)
+//     }
+// }else{
+//     for(let item of textNor){
+//      console.log(item)
+//     }
+// }
+
+
+// if(window.innerWidth < 1200){
     
-})
-noButton.addEventListener("click", function(){
-    if(document.getElementById('noHeroText').hidden = true){
-        document.getElementById('enHeroText').hidden = true
-        document.getElementById('noHeroText').hidden = false
-        document.getElementById('enMainText').hidden = true
-        document.getElementById('noMainText').hidden = false
-    }
-})
-
-// let hasScrolled = false
-// let isOnHeader = true
-
-// if(window.innerWidth < 1200 && isOnHeader){
-//     isOnHeader = false
-//     while (!hasScrolled) {
-//         onscroll = (event) => {
-//             hasScrolled = true;
-//             console.log("Element scroll event fired!");
-//             const element = document.querySelector("main");
-//             element.scrollIntoView({
-//               block: "start",
-//               behavior: "smooth",
-//               inline: "start",
-//             });
-           
-//             // setTimeout(() => {
-//             //   }, 1000);
-//         }
-//     } 
+//         window.addEventListener("scroll", function(){
+            
+//                 hasScrolled = true;
+//                 console.log("Element scroll event fired!");
+//                 const element = document.querySelector("main");
+//                 element.scrollIntoView({
+//                   block: "start",
+//                   behavior: "smooth",
+//                   inline: "start",
+//                 }
+//                 )
+               
+//                 // setTimeout(() => {
+//                 //   }, 1000);
+            
+//         }    
 // }
